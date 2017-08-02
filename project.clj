d5756de3ce81d53748f681b918fc8654a1c86d8c
Reinout Stevens <reinout@xaop.com>
2017-08-02 14:17:17 +0200

    remove graph dependency

--- project.clj
+++ project.clj
@@ -6,6 +6,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.4"]
                  [org.clojure/data.json "0.2.6"]
-                 [chartkick "0.1.0"]
                  [clj-http "3.6.1"]
                  ])
